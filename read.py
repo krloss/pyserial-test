90e2a31d7775aa430ca3a83b7792c4825267a74c
Carlos Henrique <krloshps@gmail.com>
2018-04-14 11:37:38 -0300

    Update read.py

--- read.py
+++ read.py
@@ -5,7 +5,7 @@ from serial import Serial
 # Serial<id=0xa81c10, open=False>(port='COM1', baudrate=19200, bytesize=8, parity='N', stopbits=1, timeout=None, xonxoff=0, rtscts=0)
 # timeout = integer in seconds
 serial = Serial(port=argv[1])
-text =  serial.read(int(argv[2])) if 3 == len(argv) else serial.readline()
+text = serial.read(int(argv[2])) if 3 == len(argv) else serial.readline()
 
 serial.close()
-print '>>> %s.\n' % (text)
+print '>>> %s!\n' % (text)
